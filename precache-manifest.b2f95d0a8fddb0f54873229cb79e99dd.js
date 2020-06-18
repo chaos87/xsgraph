68574e30de3c24e5db65e339403b17da8e2381d9
chaos87 <chaos87@users.noreply.github.com>
2020-06-18 03:47:55 +0000

    deploy: 12499c9293fdbc821b19e572ece3f58f1c06423b

---
+++ precache-manifest.b2f95d0a8fddb0f54873229cb79e99dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc15b3e275443ffc10a37cdb88de378",
+    "url": "/xsgraph/index.html"
+  },
+  {
+    "revision": "01692da1d33f0072abff",
+    "url": "/xsgraph/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "95e89886af766fbcea3a",
+    "url": "/xsgraph/static/css/main.42109c0f.chunk.css"
+  },
+  {
+    "revision": "01692da1d33f0072abff",
+    "url": "/xsgraph/static/js/2.5d3cd21c.chunk.js"
+  },
+  {
+    "revision": "637299647729ce14c011d40152fa8392",
+    "url": "/xsgraph/static/js/2.5d3cd21c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e89886af766fbcea3a",
+    "url": "/xsgraph/static/js/main.0e0e3c7d.chunk.js"
+  },
+  {
+    "revision": "385ac32aacb1f6e3398f",
+    "url": "/xsgraph/static/js/runtime-main.d2389818.js"
+  }
+]);
